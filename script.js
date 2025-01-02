85543fd8f301024ec5233b56e9b4f19b081a77dc
vodo <vvoodoo32@gmail.com>
2025-01-02 18:18:11 +0200

    feat: update with popup;

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-// Когда страница загружена
-window.onload = function() {
-    // Через 3 секунды показать поп-ап
-    setTimeout(function() {
-        // Показываем поп-ап
-        const popup = document.getElementById('popup');
-        popup.style.display = 'flex'; 
-        setTimeout(function() {
-            popup.style.opacity = '1'; 
-            popup.style.transform = 'translateY(0)';  
-        }, 10); 
-    }, 1000); 
+// // Когда страница загружена
+// window.onload = function() {
+//     // Через 3 секунды показать поп-ап
+//     setTimeout(function() {
+//         // Показываем поп-ап
+//         const popup = document.getElementById('popup');
+//         popup.style.display = 'flex'; 
+//         setTimeout(function() {
+//             popup.style.opacity = '1'; 
+//             popup.style.transform = 'translateY(0)';  
+//         }, 10); 
+//     }, 1000); 
     
-    // Событие для закрытия поп-апа
-    document.getElementById('popup-close').onclick = function() {
-        document.getElementById('popup').style.display = 'none';
-    }
-}
+//     // Событие для закрытия поп-апа
+//     document.getElementById('popup-close').onclick = function() {
+//         document.getElementById('popup').style.display = 'none';
+//     }
+// }
